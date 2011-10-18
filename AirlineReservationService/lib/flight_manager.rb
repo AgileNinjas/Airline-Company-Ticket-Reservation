63421e590704e6350fc2e2e465bc7cc05cf83984
Gen Krang <chukwukg@cs.man.ac.uk>
2011-10-18 18:02:12 +0100

    unfinished search method

--- AirlineReservationService/lib/flight_manager.rb
+++ AirlineReservationService/lib/flight_manager.rb
@@ -1,4 +1,8 @@
 require "../lib/route.rb"
+require "rexml/document"
+require "../lib/query"
+include REXML  # so that we don't have to prefix everything with REXML::...
+
 class Flight_manager
   attr_accessor :flights
         def initialize
@@ -9,7 +13,21 @@ class Flight_manager
     @flights.push(flight)
   end
   def search(query)
-    
+  file = File.new( "../../XMLSchema/TestSchedule.xml" )
+  doc = REXML::Document.new file
+
+
+  XPath.each( doc, "//flight") do |element|
+   if XPath.match(element,"//id")== query.arrival_city
+     puts "yes"
+   end
   end
-  
+
+
+  end
+
 end
+
+fls=Flight_manager.new
+    q=Query.new("uyu","hjhg",12-11-2010,11-10-2011)
+         fls.search(q)
