efa6270c01fa103ad7ee7c505f40d80ee8c3a382
mircea moise <mircea.moise@gmail.com>
2011-10-18 11:50:24 +0100

    Capacity , economic class , first class

--- AirlineReservationService/lib/flight.rb
+++ AirlineReservationService/lib/flight.rb
@@ -1,7 +1,11 @@
 class Flight
-attr_accessor :id , :capacity
+attr_accessor :id , :capacity , :price
+attr_accessor :first_class_capacity , :economic_class_capacity
  def initialize
    @id = 2
    @capacity = 120
+   @price = 100
+   @first_class_capacity = 20
+   @economic_class_capacity = 100
  end
 end
--- AirlineReservationService/spec/flight_spec.rb
+++ AirlineReservationService/spec/flight_spec.rb
@@ -6,8 +6,10 @@ describe "Flight" do
   it "flight should have an id which is a positive integer" do
 
     flight = Flight.new
+
     flight.id.should > 0
-    flight.id.kind_of?(1.class).should == true
+
+    flight.id.kind_of?(Integer).should == true
 
   end
 
@@ -18,7 +20,45 @@ describe "Flight" do
     flight.capacity.should >= 20
     flight.capacity.should <= 400
 
-    flight.id.kind_of?(1.class).should == true
+    flight.capacity.kind_of?(Integer).should == true
+
+  end
+
+   it "flight should have a price which is a positive number" do
+
+    flight = Flight.new
+
+    flight.price.should >= 0
+
+    flight.price.kind_of?(Numeric).should == true
+
+   end
+
+   it "flight should have a first class capacity which is an positive integer if it exists on the flight and 0 otherwise" do
+
+    flight = Flight.new
+
+    flight.first_class_capacity.should >= 0
+
+    flight.first_class_capacity.kind_of?(Integer).should == true
+
+   end
+
+   it "flight should have a economic class capacity which is an positive integer if it exists on the flight and 0 otherwise" do
+
+    flight = Flight.new
+
+    flight.economic_class_capacity.should >= 0
+
+    flight.economic_class_capacity.kind_of?(Integer).should == true
+
+   end
+
+   it "all seats of the flight should be either on economic or first class" do
+
+    flight = Flight.new
+
+    (flight.economic_class_capacity + flight.first_class_capacity).should == flight.capacity
 
   end
 
