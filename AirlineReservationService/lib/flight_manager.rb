da4fe1da13087367f2ebc3ecdf1df1a5c5eb24d1
Gen Krang <chukwukg@cs.man.ac.uk>
2011-10-18 12:59:16 +0100

    add and search method of flight manager class were defined

--- AirlineReservationService/lib/flight_manager.rb
+++ AirlineReservationService/lib/flight_manager.rb
@@ -1,6 +1,15 @@
+require "../lib/flight.rb"
 class Flight_manager
   attr_accessor :flights
         def initialize
           @flights=[]
         end
+  
+  def add_flight(flight)
+    @flights.push(flight)
+  end
+  def search(query)
+    
+  end
+  
 end
