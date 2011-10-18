166a72d9cd23375ca2f9fad9ea87c678b52b1f63
Gen Krang <chukwukg@cs.man.ac.uk>
2011-10-18 16:50:00 +0100

    Used instance of Query instead of mocking it

--- AirlineReservationService/spec/flight_manager_spec.rb
+++ AirlineReservationService/spec/flight_manager_spec.rb
@@ -1,6 +1,7 @@
 require "rspec"
 require "../lib/flight_manager.rb"
 require "../lib/flight.rb"
+ require "../lib/query.rb"
 
 describe "My behaviour" do
 
@@ -18,7 +19,8 @@ describe "My behaviour" do
   end
 
   it "should have a search method with an argument of type of Query and it should return a list of results" do
-    query = mock('Query')
+
+     query =Query.new("manchester","london",12-10-2011,10-03-2011)
     subject.search(query)
 
   end
