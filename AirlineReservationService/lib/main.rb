97e56153f2e1240447d305524816a7d0882c860e
mircea moise <mircea.moise@gmail.com>
2011-10-24 15:26:08 +0100

    main interface is completed

--- AirlineReservationService/lib/main.rb
+++ AirlineReservationService/lib/main.rb
@@ -4,6 +4,7 @@ require "../lib/flight"
 class Main
 
   def get_flight_id
+    puts "*******  Search a flight ********"
     puts "Please enter the flight id:"
     flight_id = Integer(gets)
   end
@@ -27,9 +28,37 @@ class Main
     end
   end
 
-  def show_interface
+
+  def show_flight_details
     id = get_flight_id
     show_details id
+  end
+
+  def show_main_menu
+    puts "***********************************"
+    puts "** Select from the menu below :  **"
+    puts "** 1.Search for a flight         **"
+    puts "** 2.Show flight details         **"
+    puts "** 3.Exit                        **"
+    puts "***********************************"
+    puts
+    action=Integer(gets)
+  end
+
+  def show_interface
+    while 1
+    action_id=show_main_menu()
+    case action_id
+      when 1
+        then
+      when 2
+        then show_flight_details
+      when 3
+        then break
+    end
+
+    end
+
   end
 
 end
